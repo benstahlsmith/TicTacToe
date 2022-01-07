40e2574a568a006d9270e8d2fc866e608ce7745f
Ben Stahlsmith <benstahlsmith@gatech.edu>
2022-01-06 21:29:53 -0500

    Created function to check for winner in the rows

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,5 @@
 def create_board(n):
-    return [[[' '] for _ in range(n)] for _ in range(n)]
+    return [[' ' for _ in range(n)] for _ in range(n)]
 
 def display_board(board):
     i = 1
@@ -17,13 +17,13 @@ def display_board(board):
         
     
 
-board = create_board(3)
+gameboard = create_board(3)
 
-display_board(board)
+display_board(gameboard)
 
-
+print(gameboard)
 def update_board(board, move, player_icon):
-    if board[move[0]-1][move[1]-1][0] != ' ':
+    if board[move[0]-1][move[1]-1] != ' ':
         print('Invalid move. That space is full')
         return board
     else:
@@ -31,8 +31,32 @@ def update_board(board, move, player_icon):
         return board
 
 
-new_board = update_board(board, [1,1], 'X')
+new_board = update_board(gameboard, [1,1], 'X')
+
 
 display_board(new_board)
 
+print(new_board)
 
+def check_for_winner(board):
+    print(board)
+    n = len(board)
+    for row in board:
+        for i in range(1,n):
+            if row[i] == row[i-1]:
+                if i == n - 1:
+                    print(row[i] + ' is the winner')
+                    return True
+                i += 1
+                continue
+            else:
+                break
+
+check_for_winner(new_board)
+
+new_board = update_board(new_board, [1,2], 'X')
+new_board = update_board(new_board, [1,3], 'X')
+
+display_board(new_board)
+
+check_for_winner(new_board)
